3d78350bca63edc89d178dea04092a4993e9c1ed
Jeff Tagle <jeffreyrftagle@gmail.com>
2023-08-29 19:08:32 -0400

    Started project

---
+++ Package.py
@@ -0,0 +1,5 @@
+class Package:
+
+    def __init__(self, key, value):
+        self.key = keys
+        self.value = value
---
+++ Truck.py
@@ -0,0 +1,6 @@
+class Truck:
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
