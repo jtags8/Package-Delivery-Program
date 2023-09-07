03d66fa76c12d60099bc362ca5e1b2a1902edf20
Jeff Tagle <jeffreyrftagle@gmail.com>
2023-09-07 14:55:13 -0400

    Fixed program

--- README.md
+++ README.md
@@ -4,19 +4,23 @@ Section F:
 F.  Justify the package delivery algorithm used in the solution as written in the original program by doing the following:
 1.  Describe two or more strengths of the algorithm used in the solution.
 -The nearest neighbor algorithm is simple to implement compared to other complex algorithms.
--The nearest neighbor algorithm is suitable for non-linear and random data such as determining the next shortest distance out of remaining distances.
+-The nearest neighbor algorithm is efficient.
+-Near optimal algorithm.
 
 2.  Verify that the algorithm used in the solution meets all requirements in the scenario.
 -The algorithm used in my solution does meet all requirements for the scenario.
 
 3.  Identify two other named algorithms that are different from the algorithm implemented in the solution and would meet all requirements in the scenario.
--Two other named algorithms include Dijkstra's algorithm and greedy algorithm.
+-Two other named algorithms include farthest insertion algorithm and greedy algorithm.
 
 a.  Describe how both algorithms identified in part F3 are different from the algorithm used in the solution.
-Greedy algorithm takes into account the most optimal solution at a certain point in time, but may not lead to the most optimal path.
-Dijkstra's algorithm starts at an initial vertex, then finds the shortest path to the next vertex, and keeps repeating until it reaches the final vertex. 
-In addition, a pointer to the previous vertex is taken. The algorithm terminates once all the vertices are visited.
-
+-Greedy algorithm first calculates all possible distances (edges) between any two address (vertices) and adds the shortest distance. This process repeats until a 
+cycle containing all the locations is created. This is different from the nearest neighbor algorithm because the nearest neighbor only compares the distances from the current vertex to
+all possible destination vertices.
+-Farthest insertion algorithm is a type of insertion algorithm that adds new destinations in a delivery route as the number of destinations in the route grows. 
+It first determines a destination that is the further from any one address already visited, and then it is placed between two destinations in the route that leads 
+to the shortest possible delivery route. This is different from the nearest neighbor algorithm because nearest neighbor is sub-optimal and does not lead to the shortest route,
+whereas the farthest insertion algorithm yields the shortest possible route under the algorithm's functionality.
 
 Section G:
 If I were to do the project different, I would like to use package destination address as the hash key to make use of the hash function of the HashMap. Despite not 
@@ -24,9 +28,21 @@ having to use packageID as the key, if I were to use the destination address as 
 values of the key-value pairs. This would make it easy to identify those packages that can be delivered together. In addition, I would create a GUI to compliment the
 program and built upon the CLI that is originally required.
 
-H. There are other data structures I can use other than HashMap. I could use a linked list and a binary search tree.
-Linked lists are similar to arrays, but instead make use of pointers to denote the next piece of data that is accessed. Unlike arrays, linked lists can be in random order
-in memory. This data structure has an O(n) time complexity.
-Binary search trees make use of a tree structure and has an ordering property. All values that are less than the node are placed to the left of three, whereas all values that are greater
-than the node are placed to the right of the tree. In practice, searching through a binary search tree is efficient because the set of data that is being searched is 
-continuously cut in half until the value is found, therefore has an O(log n) time complexity.
+Section H:
+There are other data structures I can use other than HashMap. I could use a linked list and a binary search tree.
+
+H1A. Describe how each data structure identified in H1 is different from the data structure used in the solution.
+
+-Linked lists are similar to arrays, but instead make use of pointers to denote the next piece of data or node in the list. 
+This allows the linked list data to be stored in memory at random. In contrast, HashMaps utilize storing data as key-value pairs. 
+It is typically more efficient to retrieve a specific data point with a HashMap compared to a linked list. In addition, 
+HashMap uses less memory than a linked list. Linked lists data structure has an O(1) time complexity for insertion and 
+deletion at the beginning or end of the list, but it has an O(n) time complexity for any insertion, removal, or lookup function. 
+In contrast, HashMap has a O(n) time complexity as worst-case, but does have an average time complexity of O(1).
+-Binary search trees (BSTs) make use of a tree structure and has an ordering property. All values that are less than the 
+node are placed to the left of three, whereas all values that are greater than the node are placed to the right of the tree. 
+In contrast, the HashMap, as stated above, utilizes key-value pairs to store data. Data is retrieved by using the key to return the value.
+In practice, searching through a binary search tree is efficient because the set of data that is being searched is continuously 
+cut in half until the value is found, leading to an average time complexity of O(log n). In addition, insertion and deletion 
+has an O(log n) time complexity on average. Despite this, the worst case scenario of a BST is O(n) if the BST is unbalanced. 
+The time complexity again for a HashMap is O(n) time complexity as worst-case, but does have an average time complexity of O(1).
--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def get_address_index(address, address_list):
 # the list of package IDs, retrieves the Package object of that ID, and then retrieves the delivery address of the package.
 # The function then retrieves the distance, compares that distance to the current min distance, and then keeps the min distance.
 # Using that min distance, the function identifies the destination address that has that distance and returns the address.
-#O(n) time complexity.
+#O(n) time complexity. Later on, the delivery function implements the nearest_neighbor function which leads to an O(n^2) time complexity.
 def nearest_neighbor(current_address, truck_packages, package_hash_list, distances, address_list):
     min_distance = float('inf')
     nearest_address_index = 0
@@ -125,7 +125,7 @@ def calc_travel_time(distance): #18 mph, can use for individual deliveries or to
 
 #Deliver function. Primary function that allows trucks to deliver packages based on nearest neighbor algorithm.
 #Returns total travel distance for the truck.
-#O(n) time complexity.
+#O(n^2) time complexity.
 def deliver(truck, packages, start_time_h, start_time_m, start_time_s, end_time_h, end_time_m, end_time_s):
     #Parameters are truck object and package hashmap and time to depart, returns total distance traveled
 
@@ -141,27 +141,27 @@ def deliver(truck, packages, start_time_h, start_time_m, start_time_s, end_time_
     delivery_time_in_sec = start_time_in_secs # initial value for total delivery time
 
     while len(truck.packages) > 0:
-        print("***Next while loop iteration***")
+        #print("***Next while loop iteration***")
         if delivery_time_in_sec >= 37200:
             if 9 in truck.packages:
                 p = packages.lookup(str(9))
                 p.update_delivery_address("410 S State St")
-        print("Current address: " + current_address)
+        #print("Current address: " + current_address)
         dest_address = nearest_neighbor(current_address, truck.packages, packages, distances_list, addresses)
-        print("Destination address: " + dest_address)
+        #print("Destination address: " + dest_address)
         #if current_address == dest_address:
             # = 0
         #else:
         current_travel_dist = float(get_distance(get_address_index(current_address, addresses), get_address_index(dest_address, addresses), distances_list))
-        print("Travel distance from current address to destination address: " + str(current_travel_dist))
+        #print("Travel distance from current address to destination address: " + str(current_travel_dist))
         total_dist += current_travel_dist
-        print("Total distance so far: " + str(total_dist))
+        #print("Total distance so far: " + str(total_dist))
         travel_time = calc_travel_time(current_travel_dist)
         test_time = datetime.timedelta(seconds=travel_time)
-        print("Time to travel to current dest: " + str(test_time))
+        #print("Time to travel to current dest: " + str(test_time))
         delivery_time_in_sec += travel_time
         delivery_time = datetime.timedelta(seconds=delivery_time_in_sec)
-        print("Time of delivery: " + str(delivery_time))
+        #print("Time of delivery: " + str(delivery_time))
 
         if delivery_time_in_sec > total_end_time_in_secs:
             return total_dist
@@ -176,15 +176,15 @@ def deliver(truck, packages, start_time_h, start_time_m, start_time_s, end_time_
         current_address = dest_address
 
     return_home_dist = float(get_distance(get_address_index(current_address, addresses), get_address_index("4001 South 700 East", addresses), distances_list))
-    print("Current address: " + current_address)
-    print("Return to hub distance :" + str(return_home_dist))
+    #print("Current address: " + current_address)
+    #print("Return to hub distance :" + str(return_home_dist))
     total_dist += return_home_dist
-    print("Total distance traveled by truck: " + str(total_dist))
+    #print("Total distance traveled by truck: " + str(total_dist))
 
     return total_dist
 
 #Function used to start the program
-#O(n) time complexity
+#O(n^2) time complexity for the overall program.
 def start():
     print("Welcome to WGUPS Service! Please choose one of the following options:")
 
@@ -192,15 +192,16 @@ def start():
         reset_trucks(truck1, truck2, truck3)
         create_package_hashmap(new_package_list, package_hashmap)
 
-        truck1_total_travel_time = 0
-        truck2_total_travel_time = 0
-        truck3_total_travel_time = 0
+        #truck1_total_travel_time = 0
+        #truck2_total_travel_time = 0
+        #truck3_total_travel_time = 0
         truck1_delivery = 0
         truck2_delivery = 0
         truck3_delivery = 0
-        end_time_in_secs = 0
+        #end_time_in_secs = 0
         print("0 - Exit")
         print("1 - Status of all packages at a given time")
+        print("2 - Status of one single package")
 
         x = input()
         if x == "0":
@@ -222,8 +223,8 @@ def start():
                 truck2_delivery = deliver(truck2, package_hashmap, 9, 5, 0, time_input_hours, time_input_mins,
                                           time_input_secs)
 
-            if end_time_in_secs >= 37540:
-                truck3_delivery = deliver(truck3, package_hashmap, 10, 25, 40, time_input_hours, time_input_mins,
+            if end_time_in_secs >= 36340:
+                truck3_delivery = deliver(truck3, package_hashmap, 10, 5, 40, time_input_hours, time_input_mins,
                                           time_input_secs)
 
             truck1_total_travel_time_sec = calc_travel_time(truck1_delivery)
@@ -233,17 +234,46 @@ def start():
             truck3_total_travel_time_sec = calc_travel_time(truck3_delivery)
             truck3_total_travel_time = datetime.timedelta(seconds=truck3_total_travel_time_sec)
 
-        print("Status for packages as of " + str(datetime.timedelta(seconds=end_time_in_secs)))
-        for i in range(1, 41):
-            print(package_hashmap.lookup(str(i)))
+            print("Status for packages as of " + str(datetime.timedelta(seconds=end_time_in_secs)))
+            for i in range(1, 41):
+                print(package_hashmap.lookup(str(i)))
 
-        print("Total distance traveled for Truck 1 in miles: " + str(truck1_delivery))
-        print("Total distance traveled for Truck 2 in miles: " + str(truck2_delivery))
-        print("Total distance traveled for Truck 3 in miles: " + str(truck3_delivery))
-        print("Total distance traveled for all three trucks: " + str(truck1_delivery + truck2_delivery + truck3_delivery))
-        print("Total time traveled for Truck 1: " + str(truck1_total_travel_time))
-        print("Total time traveled for Truck 2: " + str(truck2_total_travel_time))
-        print("Total time traveled for Truck 3: " + str(truck3_total_travel_time))
+            print("Total distance traveled for Truck 1 in miles: " + str(truck1_delivery))
+            print("Total distance traveled for Truck 2 in miles: " + str(truck2_delivery))
+            print("Total distance traveled for Truck 3 in miles: " + str(truck3_delivery))
+            print("Total distance traveled for all three trucks: " + str(truck1_delivery + truck2_delivery + truck3_delivery))
+            print("Total time traveled for Truck 1: " + str(truck1_total_travel_time))
+            print("Total time traveled for Truck 2: " + str(truck2_total_travel_time))
+            print("Total time traveled for Truck 3: " + str(truck3_total_travel_time))
+
+        if x == "2":
+            print("Give the package ID to search:")
+            package_id_input = int(input())
+            print("Give the hours in a 24 hr format")
+            time_input_hours = int(input())
+            print("Give the amount of minutes")
+            time_input_mins = int(input())
+            print("Give the amount of seconds")
+            time_input_secs = int(input())
+            end_time_in_secs = time_input_hours * 3600 + time_input_mins * 60 + time_input_secs
+
+            if end_time_in_secs >= 28800:
+                deliver(truck1, package_hashmap, 8, 0, 0, time_input_hours, time_input_mins,
+                                          time_input_secs)
+
+            if end_time_in_secs >= 32700:
+                deliver(truck2, package_hashmap, 9, 5, 0, time_input_hours, time_input_mins,
+                                          time_input_secs)
+
+            if end_time_in_secs >= 36340:
+                deliver(truck3, package_hashmap, 10, 5, 40, time_input_hours, time_input_mins,
+                                          time_input_secs)
+
+            print("Status for package " + str(package_id_input) + " as of " + str(datetime.timedelta(seconds=end_time_in_secs)))
+            print(package_hashmap.lookup(str(package_id_input)))
+        else:
+            print("Invalid input. Please enter one of the following options (0, 1, 2).")
+
 
 
 start()
